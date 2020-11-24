8ecc8ad16af350a5eef424db26a7702eaff270f6
MilenaGkm <milenagaranin@gmail.com>
2020-11-24 12:15:53 +0200

    api

--- server.js
+++ server.js
@@ -11,9 +11,20 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
 
+app.get('/sanity', function(req, res) {
+    res.send("OK")
+})
+
+app.get('/movies/:title', function(req, res) {
+	const movieName = req.params.title;
+	urllib.request(`http://www.omdbapi.com/?apikey=a2e2ca53&t=${movieName}`, function(err,response) {
+		const movieData = JSON.parse(response.toString());
+		res.send(movieData);
+	})
+})
 
 
-const port = 3000
+const port = 8080
 app.listen(port, function(){
     console.log(`Running server on port ${port}`)
 })
